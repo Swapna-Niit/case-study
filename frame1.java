8d7b1f88884da81373182451a5246efad0e064dc
Arun Kumar Reddy Vemireddy <40706283+arunvemireddy@users.noreply.github.com>
2019-10-22 10:20:43 +0530

    Create frame1.java

---
+++ frame1.java
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<img  src="C:\Users\arunkumarrv\eclipse-workspace\CASESTUDYDWP\WebContent\Capture1.PNG" alt="alternate"
+style="width:800px;height:110px"/>
+</body>
+</html>
